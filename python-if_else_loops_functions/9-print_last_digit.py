b403ffbd080081545de4f31de67d2734fff1c0f9
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-01-26 14:46:40 -0400

    Last digit func upload

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,8 @@ def print_last_digit(number):
     if number < 0:
         ldigit = -number % 10
         ldigit = -ldigit
-    else:
+    elif number > 0:
         ldigit = number % 10
+    else:
+        ldigit = 0
 print(ldigit, end="")
