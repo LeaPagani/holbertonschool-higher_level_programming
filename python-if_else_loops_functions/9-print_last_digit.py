0c25d349abe75b7626af67f66a572058f5a5d689
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-01-26 14:50:23 -0400

    Adding abs func

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        ldigit = -number % 10
-        ldigit = -ldigit
-    elif number > 0:
-        ldigit = number % 10
-    else:
-        ldigit = 0
-print(ldigit, end="")
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
