84cc17824c6134a98f22d9e23cb72fdd5cfa977c
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-01-26 09:39:56 -0400

    Fixing print format

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,6 @@ if ldigit > 5:
 if ldigit > 5:
     print(f"Last digit of {number} is {ldigit} and is greater than 5")
 elif ldigit < 6 and ldigit != 0:
-    print("Last digit of {number} is {ldigit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {ldigit} and is less than 6 and not 0")
 elif ldigit == 0:
-    print("Last digit of {number} is {ldigit} and is 0")
+    print(f"Last digit of {number} is {ldigit} and is 0")
