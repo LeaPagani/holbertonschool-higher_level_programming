9ab2b7eb7838f731db5ba3e38ff109ef8cbf8833
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-01-26 10:51:51 -0400

    Print comb2 func

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+newend = ", "
+for i in range(0, 100):
+    if i < 10:
+        print("{}{}".format(0, i), end=newend)
+    else:
+        if i == 99:
+            newend = "\n"
+        print("{}{}".format(0, i), end=newend)
