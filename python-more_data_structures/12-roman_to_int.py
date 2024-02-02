72eb07e67f4fbe0275400135f7046833f1c90f75
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-02 19:39:10 -0400

    Solving for IIV case

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_values = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'M' : 1000}
+    roman_values = {
+        'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'M' : 1000
+    }
     result = 0
     prev_value = 0
+    prev_biggestvalue = 0
     if type(roman_string) is str and roman_string:
         for iteration in range(len(roman_string) - 1, -1, -1):
-            if roman_values[roman_string[iteration]] >= prev_value:
+            if roman_values[roman_string[iteration]] > prev_value:
+                result += roman_values[roman_string[iteration]]
+                prev_value = roman_values[roman_string[iteration]]
+            elif roman_values[roman_string[iteration]] == prev_value:
                 result += roman_values[roman_string[iteration]]
             else:
                 result -= roman_values[roman_string[iteration]]
-            prev_value = roman_values[roman_string[iteration]]
+            
     return result
