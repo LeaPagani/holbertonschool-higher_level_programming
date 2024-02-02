dfdf5497f3350b3e4a4d552ccd5d6d95cc9cb1bd
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-02 18:59:08 -0400

    Changing iteration to reverse

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,9 @@
 def roman_to_int(roman_string):
     roman_values = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'M' : 1000}
     result = 0
-    previous = 0
+    prev_value = 0
     if type(roman_string) is str and roman_string:
-        for iteration in roman_string:
+        for iteration in range(len(roman_string) - 1, -1, -1):
             if roman_values[roman_string[iteration]] >= prev_value:
                 result += roman_values[roman_string[iteration]]
             else:
