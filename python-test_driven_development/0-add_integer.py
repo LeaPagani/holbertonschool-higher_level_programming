9e501b08ac6761a24aa4f4e49ffa47aba8a9049f
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-05 17:55:51 -0400

    New function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Add int function"""
-def add_integer(a, b=98):
-    """Function that returns the sum of two integers"""
+"""integer addition function"""
 
-    if not isinstance(a, (int, float)):
-        raise TypeError('a must be an integer')
-    if not isinstance(b, (int, float)):
-        raise TypeError('b must be an integer')
-    else:
-        return int(a) + int(b)
+
+def add_integer(a, b=98):
+    """adds two integers"""
+    if not(isinstance(a, int)) and not(isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not(isinstance(b, int)) and not(isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return ((int(a)) + (int(b)))
