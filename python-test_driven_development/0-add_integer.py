30543993bc1aa35bf76c3eba4e2da1e5fbe69bb5
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-05 17:42:33 -0400

    Adding isinstance function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,9 +3,9 @@
 def add_integer(a, b=98):
     """Function that returns the sum of two integers"""
 
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
-    if type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
     else:
         return int(a) + int(b)
