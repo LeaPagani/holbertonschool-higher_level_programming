3efa50feb2fa3735e557d8d4f642a2a275ffab3c
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-05 18:05:00 -0400

    Editing test file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,8 +4,8 @@
 
 def add_integer(a, b=98):
     """adds two integers"""
-    if not(isinstance(a, int)) and not(isinstance(a, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not(isinstance(b, int)) and not(isinstance(b, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("b must be an integer")
     return ((int(a)) + (int(b)))
