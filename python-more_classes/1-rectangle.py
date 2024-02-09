287390808eb78900a56974fc35bf32e02b5fae4d
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-09 14:12:33 -0400

    Rectangle class

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""Rectangle class"""
+"""Docstring for file"""
 
-class Reactangle __init__(self, width=0, heigth=0):
-    self._width = width
-    self._heigth = heigth
 
-@property 
+class Rectangle __init__(self, width=0, heigth=0):
+    """Docstring for rectangle class"""
+    self.width = width
+    self.heigth = heigth
+
+@property
 def width(self):
-    if type(width) isinstance(int):
-        if width > 0:
-            self._width = width
+    """Docstring for Rectangle width"""
+    return self._width
+
+@width.setter
+def width (self, value):
+    """Docstring for Rectangle width setter"""
+    if type(value) is int:
+        if value > 0:
+            self._width = value
         else:
             raise ValueError (width must be >= 0)
     else:
@@ -17,14 +25,16 @@ def width(self):
 
 @property 
 def heigth(self):
-    if type(heigth) isinstance(int):
-        if heigth > 0:
-            self._width = heigth
+    """Docstring for Rectangle heigth"""
+    return self._heigth
+
+@heigth.setter
+def heigth (self, value):
+    """Docstring for Rectangle heigth setter"""
+    if type(value) is int:
+        if value > 0:
+            self._heigth = value
         else:
             raise ValueError (heigth must be >= 0)
     else:
         raise TypeError (heigth must be an integer)
-
-
-
-
