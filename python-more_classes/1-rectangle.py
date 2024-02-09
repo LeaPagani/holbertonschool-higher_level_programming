2dcb57f165c7acec8294119ba0fc634c0d927fc9
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-09 15:40:14 -0400

    Pycodestyle verification

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,11 +5,14 @@ This module contains a class that defines a rectangle.
 
 
 class Rectangle ():
+    """Defines a rectangle."""
+
 def __init__(self, width=0, heigth=0):
     """Sets the necessary attributes for the Rectangle object.
         Args:
             width (int): the width of the rectangle.
-            height (int): the height of the rectangle."""
+            height (int): the height of the rectangle.
+    """
     self.width = width
     self.heigth = heigth
 
