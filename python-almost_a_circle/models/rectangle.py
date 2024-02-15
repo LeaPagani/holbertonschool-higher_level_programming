b5102386e41d7e90ee93df8b6588fc8f429803b7
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-15 19:34:31 -0400

    adding documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,11 +14,19 @@ class Rectangle(Base):
         __y (int): the vertical (y) padding of the rectangle.
     """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes the default attributes of the Base object.
+        Args:
+            width (int): the wanted width of the rectangle.
+            height (int): the wanted height of the rectangle.
+            x (int): the wanted horizontal (x) padding of the rectangle.
+            y (int): the wanted vertical (y) padding of the rectangle.
+            id (int): the wanted identifier of the Base object.
+        """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
