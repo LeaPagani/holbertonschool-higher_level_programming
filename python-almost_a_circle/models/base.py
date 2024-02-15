328ffe5fca517ffa1e38aaca028911459ea05542
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-14 20:08:11 -0400

    adding documentation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 class Base():
-    """ Base class """
+    """ This class will be the “base” of all other classes in this project. 
+    The goal of it is to manage id attribute in all your future classes and 
+    to avoid duplicating the same code (by extension, same bugs) """
 
     __nb_objects = 0
     """ Private class Atribute """
 
     def __init__(self, id=None):
         """ Class constructor
-        Args: id(int) """
-
+        Args: 
+        id(int) 
+        """
         if id is not None:
             self.id = id
         else:
