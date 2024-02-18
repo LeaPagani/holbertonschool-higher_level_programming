b2d6dff74194510e58a82ce3b80f4465acf9fb0e
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-17 20:38:54 -0400

    return list of instances method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -69,3 +69,20 @@ class Base():
 
         dummy_instance.update(**dictionary)  # Update dummy with dictionary
         return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances loaded from a JSON file."""
+        file_name = cls.__name__ + ".json"  # Construct the file name based on class name
+        instances = []
+
+        try:
+            with open(file_name, 'r') as file:
+                json_data = file.read()  # Read the JSON data from the file
+                if json_data:
+                    dicts = cls.from_json_string(json_data)
+                    instances = [cls.create(**d) for d in dicts]
+        except FileNotFoundError:
+            pass  # If the file doesn't exist, return an empty list
+
+        return instances
