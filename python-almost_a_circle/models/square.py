13b22c478bced9669621e98498cba0cfd42866b3
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-17 20:03:26 -0400

    correcting dictionary method error

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -38,6 +38,6 @@ class Square(Rectangle):
     def to_dictionary(self):
         """Returns the dictionary representation of a Square"""
         return {
-            'id': self.id, 'size': self.size, 'height': self.height,
+            'id': self.id, 'size': self.size,
             'x': self.x, 'y': self.y
         }
